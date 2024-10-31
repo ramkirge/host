0760ff7d53145d8e1c860c7603a5ced8234cf736
ram kirge <kirgeram489@gmail.com>
2024-10-31 11:48:20 +0530

    member update commit

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import user from '../assets/user.jpeg'
+import amitpal from '../assets/member/amitpal.jpeg'
 import userf from '../assets/user.jpeg'
 
 import { BsLinkedin } from "react-icons/bs";
@@ -11,63 +11,248 @@ function Team() {
 
   const members = [
     {
-      name : 'Viren Pardeshi',
+      name : 'VIREN PARDESHI',
       position : 'Captain',
-      education : 'Mechanical Engineering, 3rd year',
+      education : 'Mechanical Engineering, Third year',
       linkidn : 'iiuhihiu',
       profile : require('../assets/user.jpeg')
 
     },
     {
-      name : 'Yash Kshirsagar',
+      name : 'YASH KSHIRSAGAR',
       position : 'Vice Captain ',
-      education : 'Mechanical Engineering, 4th year',
+      education : 'Mechanical Engineering, Final year',
       linkidn : 'iiuhihiu',
       profile : require('../assets/user.jpeg')
 
     },
 
     {
-      name : 'Shivam Naragude',
+      name : 'SHIVAM NARAGUDE',
       position : 'Manager',
-      education : 'Mechanical Engineering, 4th year',
-      linkidn : 'iiuhihiu',
-      profile : require('../assets/user.jpeg')
+      education : 'Mechanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/shivam-naragude-24454a321/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/shivam.jpeg')
 
     },
 
     {
-      name : 'Aditya Bhavsar',
+      name : 'ADITYA BHAVSAR',
       position : 'Powertrain System Head',
-      education : 'Electronics and Communication Engineering, 3rd year',
-      linkidn : 'iiuhihiu',
+      education : 'Electronics and Telecommunication Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/aditya-bhavsar-7bb637235/u',
+      profile : require('../assets/member/aditya.jpg')
+
+    },
+    {
+      name : 'RAM KIRGE',
+      position : 'Electrical System Head',
+      education : 'Computer Science and Engineering, Second year',
+      linkedin : 'iiuhihiu',
       profile : require('../assets/user.jpeg')
 
     },
     {
-      name : 'Ram Kirge',
-      position : 'Electrical System Head',
-      education : 'Computer Science and Engineering, 2nd year',
-      linkidn : 'iiuhihiu',
-      profile : require('../assets/user.jpeg')
+      name : 'ROHAN BHADARGE',
+      position : 'Steering System Head',
+      education : 'Mecjhanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/rohan-bhadarge-63605b229/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
 
     },
     {
-      name : 'Ram Kirge',
-      position : 'Electrical System Head',
-      education : 'Computer Science and Engineering, 2nd year',
-      linkidn : 'iiuhihiu',
-      profile : require('../assets/user.jpeg')
+      name : 'AKANSHA SALVE',
+      position : 'Breaking System Head',
+      education : 'Mechanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/akansha-salve-3bb0782b8/',
+      profile : require('../assets/member/userf.jpeg')
 
     },
     {
-      name : 'Ram Kirge',
-      position : 'Electrical System Head',
-      education : 'Computer Science and Engineering, 2nd year',
-      linkidn : 'iiuhihiu',
-      profile : require('../assets/userf.jpeg')
+      name : 'SOHAM MAHATOLE',
+      position : 'Sopnsorship Team Head',
+      education : 'Computer Science and Engineering (AIML), Second year',
+      linkedin : 'https://www.linkedin.com/in/soham-mahatole-69b802262/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
 
-    }
+    },
+
+    {
+      name : 'POOJA DHOOT',
+      position : 'Powertrain System Member',
+      education : 'Electronics and Telecommunication Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/pooja-dhoot-a6789b327/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : ' MRUNAL BHALERAO',
+      position : 'Powertrain System Member',
+      education : 'Electronics and Telecommunication Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/mrunal-bhalerao-8a172b302/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'GAYATRI BAHUDHANE',
+      position : 'Powertrain System Member',
+      education : 'Electronics and Telecommunication Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/gayatri-bahudhane-5a3b7a2b3/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'TANISHKA CHAVAN',
+      position : 'Powertrain System Member',
+      education : 'Electronics and Telecommunication Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/tanishka-chavan-624ab3335/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'SHRUSHTI RAMPURIKAR',
+      position : 'Electric System Member',
+      education : 'Electronics and Telecommunication Engineering, First year',
+      linkedin : 'https://www.linkedin.com/in/shrushti-rampurikar-83999b335/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'PRASAD AKHADE',
+      position : 'Steering System Member',
+      education : 'Mechanical Engineering, Second year',
+      linkedin : 'https://www.linkedin.com/in/soham-mahatole-69b802262/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/prasad.jpg')
+
+    },
+
+    {
+      name : 'KAIF PATHAN',
+      position : 'Steering System Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/kaif-pathan-a31509264/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'YASH BIRARI',
+      position : 'Steering System Member',
+      education : 'Mechanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/yash-birari-822773278/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'TEJAS RATHOD',
+      position : 'Breaking System Member',
+      education : 'Mechanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/tejas-rathod-16a8432b7/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    
+
+    {
+      name : 'SAHIL GOLE',
+      position : 'Breaking System Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/sahil-gole-21030b22b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'VIVEK CHAVAN',
+      position : 'Suspension System Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/vivek-chavan-91a20428a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'SARTHAK CHAVAN',
+      position : 'Suspension System Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/sarthak-chavan-3a8302326/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+    {
+      name : 'ARATI REVGADE',
+      position : 'Suspension System Member',
+      education : 'Mechanical Engineering, Final year',
+      linkedin : 'https://www.linkedin.com/in/arti-revgade-7b45202ba/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'RUSHIKESH DARWANTE',
+      position : 'Suspension System Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/rushi-darwante-a68a85335?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'KRUSHNA BHAWAR',
+      position : 'Design Team Member',
+      education : 'Mechanical Engineering, Third year',
+      linkedin : 'https://www.linkedin.com/in/rushi-darwante-a68a85335?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+
+    {
+      name : 'KULPREET KAUR SHERMAR',
+      position : 'Sponsorship Team Member',
+      education : 'Computer Science and Engineering, Second year',
+      linkedin : 'https://www.linkedin.com/in/kulpreet-kaur-shermar-b75160326/',
+      profile : require('../assets/member/kulpreet.jpg')
+
+    },
+
+    {
+      name : 'OM CHANNE',
+      position : 'Sponsorship Team Member',
+      education : 'Computer Science and Engineering (AIML), Second year',
+      linkedin : 'https://www.linkedin.com/in/om-channe-90868328b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      profile : require('../assets/member/user.jpeg')
+
+    },
+    {
+      name : 'SUKANYA PURANIK',
+      position : 'Sponsorship Team Member',
+      education : 'Computer Science and Engineering (AIML), Second year',
+      linkedin : 'https://www.linkedin.com/in/sukanya-puranik-70385a306/',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+    {
+      name : 'SIDDHI KONARDE',
+      position : 'Sponsorship Team Member',
+      education : 'Electronics and Telecommunication Engineering, Second year',
+      linkedin : 'https://www.linkedin.com/in/siddhi-konarde-982105336/',
+      profile : require('../assets/member/userf.jpeg')
+
+    },
+
+
+
+
+    
+
 
   ]
 
@@ -93,11 +278,14 @@ function Team() {
          className='p-5 bg-black border-b-2 px-8 lg:w-1/4  h-auto border-red-600 hover:border-2  transition duration-300 '>
           
           <div className='flex flex-col'>
-            <div className='p-5'><img className='h-80' src={user} alt="" /></div>
-            <div className='flex flex-col space-y-5 text-center p-4'>
-              <h2 className='text-2xl'>Prof. Amitpalsingh Punewala</h2>
+            <div className='p-5'><img className='h-80' src={amitpal} alt="" /></div>
+            <div className='flex justify-center items-center flex-col space-y-5 text-center p-4'>
+              <h2 className='text-2xl'>PROF. AMITPALSINGH PUNEWALA</h2>
               <p>Faculty Advisor</p>
               <p>Assistant Professer,Mechanical Department</p>
+              <a href='https://www.linkedin.com/in/amitpal-singh-punewale-099b2899/' target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">
+                         <div className='flex flex-row'><h3>Linked </h3><BsLinkedin className='mt-1'/></div>
+              </a>
             </div>
           </div>
 
@@ -127,7 +315,7 @@ function Team() {
                     
                       <p>{member.education}</p>
                       <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">
-                         <div className='flex flex-row'><h3>Linked</h3><BsLinkedin className='mt-1'/></div>
+                         <div className='flex flex-row'><h3>Linked </h3><BsLinkedin className='mt-1'/></div>
                       </a>
                     </div>
                   )}
