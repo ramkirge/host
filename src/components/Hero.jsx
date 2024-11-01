bb6814f1ed9bd02b1bdec807db2ffb54c6e2d6ac
ram kirge <kirgeram489@gmail.com>
2024-11-01 12:17:53 +0530

    manit upd   commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,18 +7,18 @@ function Hero() {
   return (
         
         <Element name="hero">
-          <div className='text-gray-200 bg-black/40 flex flex-col items-center justify-center  space-y-0 justify-center w-screen h-screen items-center'>
+          <div className='text-gray-200 bg-black/40 flex flex-col items-center justify-center  space-y-6 justify-center w-screen h-screen items-center'>
             
-            <div className='flex flex-col items-center justify-center space-x-6   w-screen h-screen'>
+            <div className='flex flex-col items-center justify-center   w-screen h-screen'>
               <div className='w-48'>
                 <img src={logopng} alt="" />
               </div>
-                <div className='flex space-x-8 items-center justify-center flex-col lg:flex-row'>
-                  <span className='text-5xl lg:text-8xl'>TEAM </span>
+                <div className='flex  items-center justify-center flex-col lg:flex-row'>
+                  <span className='text-5xl lg:text-8xl'>TEAM   &nbsp; </span>
                   <span className='text-5xl lg:text-8xl'>  VIDYUTASHWA</span>
                 </div>
                 <div>
-                  <h1># FORGING A LEGACY</h1>
+                  <h1 className='text-xl lg:text-2xl'># FORGING A LEGACY</h1>
                 </div>
             </div>
           
